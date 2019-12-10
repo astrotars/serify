44656f37f3c749ed4d052f2d395e7347757e2de4
Nick Parsons <nparsons08@gmail.com>
2019-12-10 08:41:34 -0700

    Remove fetch

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -25,8 +25,6 @@ export default class Fetch {
 				obj['Code'] = payload.code;
 			}
 
-			console.log(obj);
-
 			const { status, data } = await this.fetch.request({
 				method: 'POST',
 				url: path,
